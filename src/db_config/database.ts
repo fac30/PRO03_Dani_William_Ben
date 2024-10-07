652dc322a16ad924353a560ea93f9b8c56b17f20
william-man <william.man@uymail.com>
2024-10-07 17:36:47 +0100

    updated env variables for db config

--- src/db_config/database.ts
+++ src/db_config/database.ts
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
-  user: 'root',
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: 'data',
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  port: parseInt(process.env.DB_PORT ? process.env.DB_PORT : '3306'),
 });
